601e6e7d87cd69161d0fad58816f1a5f59e33b2c
Zain <zainmehar126@gmail.com>
2021-12-29 15:44:44 +0500

    after merge commit

--- githubLearning/AppDelegate.swift
+++ githubLearning/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("it is entry point of the application")
+        print("it is entry point of the application,yes")
         return true
     }
 
